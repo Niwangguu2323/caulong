2241e00758fcb9daaa5f9c2d1a83284324d28a11
Hoàng Hạnh Nhi <hoanghanhnhibmt@gmail.com>
2024-10-01 21:36:14 +0700

    Update pipelines.py

--- caulong/caulong/pipelines.py
+++ caulong/caulong/pipelines.py
@@ -16,7 +16,7 @@ import os
 
 class MongoDBCauLongPipeline:
     def __init__(self):
-        pymongo.MongoClient('mongodb://192.168.11.164:27017')
+        self.client = pymongo.MongoClient('mongodb://192.168.11.164:27017')
         self.db = self.client['dbcaulong'] #Create Database      
         pass
     def process_item(self, item, spider):
